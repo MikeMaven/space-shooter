c0ca75c8601088beba0534aa2224c64e7d05f517
MikeMaven <mikemaven@gmail.com>
2020-02-03 17:54:48 -0500

    broke out player controls into separate script

---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public float horizontalInput { get; private set; }
+    public float verticalInput { get; private set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+}
